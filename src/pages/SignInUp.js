0ff9f8b9a3f122360b6b86c283f08d2ef8f76bba
sabaarabzadegan <sabaarabzadegan@gmail.com>
2020-01-06 12:06:40 +0330

    page border removed

--- src/pages/SignInUp.js
+++ src/pages/SignInUp.js
@@ -45,8 +45,7 @@ function a11yProps(index) {
 
 const useStyles = makeStyles(theme => ({
     root: {
-        backgroundColor: theme.palette.background.paper,
-        width: 500,
+        width: 550,
         flexGrow: 1,
     },
     paper: {
