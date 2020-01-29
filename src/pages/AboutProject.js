7dff4728c444bfbafd189aa9cb43ee731ccc38ef
Mohamadhassan <emohamadhassan@gmail.com>
2020-01-29 19:04:01 +0330

    fix a typo

--- src/pages/AboutProject.js
+++ src/pages/AboutProject.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import img1 from './pic/clothes.jpg'
 import Typography from '@material-ui/core/Typography';
+import {assets} from "../values/assets";
+
 class AboutProject extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-        }
+        this.state = {}
 
 
     }
@@ -14,146 +13,146 @@ class AboutProject extends React.Component {
 
 
     render() {
-        const CONTAINER={
-            width:"100%",
-            display:"flex",
-            flexDirection:"column",
-            padding:"1%",
+        const CONTAINER = {
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            padding: "1%",
 
-        }
-        const container={
-            width:"90%",
+        };
+        const container = {
+            width: "90%",
             // height:"200px",
             //     backgroundColor:"#c7c7c7",
-            display:"flex",
-            flexDirection:"row",
-            padding:".4%",
-            alignSelf:"center",
-            justifyContent:"center",
-            alignItems:"center",
-            alignContent:"center",
-            position:"relative",
-            marginBottom:"5%",
-            marginTop:"5%",
+            display: "flex",
+            flexDirection: "row",
+            padding: ".4%",
+            alignSelf: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            position: "relative",
+            marginBottom: "5%",
+            marginTop: "5%",
 
-        }
-        const imgContainer={
-            width:"25%",
-            display:"flex",
-            flexDirection:"column",
-            alignSelf:"center",
-            justifyContent:"center",
-            alignItems:"center",
-            alignContent:"center",
-            overflow:"hidden",
+        };
+        const imgContainer = {
+            width: "25%",
+            display: "flex",
+            flexDirection: "column",
+            alignSelf: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            overflow: "hidden",
 
-        }
-        const img={
-            borderRadius:"50%",
-            width:"200px",
-            height:"200px",
-            border:"2px solid #414141",
+        };
+        const img = {
+            borderRadius: "50%",
+            width: "200px",
+            height: "200px",
+            border: "2px solid #414141",
 
 
-        }
-        const info={
-            width:"70%",
-            height:"100%",
+        };
+        const info = {
+            width: "70%",
+            height: "100%",
             // backgroundColor:"red",
-            display:"flex",
-            flexDirection:"row",
-            padding:".4%",
-            alignSelf:"center",
-            justifyContent:"center",
-            alignItems:"center",
-            alignContent:"center",
-            position:"relative",
-            marginBottom:"5%",
+            display: "flex",
+            flexDirection: "row",
+            padding: ".4%",
+            alignSelf: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            position: "relative",
+            marginBottom: "5%",
 
 
-        }
-        const explanationBox={
-            width:"46%",
-            height:"100%",
+        };
+        const explanationBox = {
+            width: "46%",
+            height: "100%",
             // backgroundColor:"blue",
-            display:"flex",
-            flexDirection:"column",
-            paddingLeft:"2%",
-            paddingRight:"2%",
-            justifyContent:"center",
-            alignItems:"center",
+            display: "flex",
+            flexDirection: "column",
+            paddingLeft: "2%",
+            paddingRight: "2%",
+            justifyContent: "center",
+            alignItems: "center",
 
 
-        }
+        };
 
 
-        const btl={
+        const btl = {
             position: "absolute",
-            width:"12%",
-            height:"3px",
-            top:"0",
-            left:"0",
-            backgroundColor:"black",
-
-        }
-        const btr={
-            position: "absolute",
-            width:"12%",
-            height:"3px",
-            top:"0",
-            right:"0",
-            backgroundColor:"black",
-
-        }
-        const bbl={
-            position: "absolute",
-            width:"12%",
-            height:"3px",
-            bottom:"0",
+            width: "12%",
+            height: "3px",
+            top: "0",
             left: "0",
-            backgroundColor:"black",
-            cursor:"pointer"
-        }
-        const bbr={
+            backgroundColor: "black",
+
+        };
+        const btr = {
             position: "absolute",
-            width:"12%",
-            height:"3px",
+            width: "12%",
+            height: "3px",
+            top: "0",
+            right: "0",
+            backgroundColor: "black",
+
+        };
+        const bbl = {
+            position: "absolute",
+            width: "12%",
+            height: "3px",
+            bottom: "0",
+            left: "0",
+            backgroundColor: "black",
+            cursor: "pointer"
+        };
+        const bbr = {
+            position: "absolute",
+            width: "12%",
+            height: "3px",
             bottom: "0",
             right: "0",
-            backgroundColor:"black",
-        }
-        const bl={
+            backgroundColor: "black",
+        };
+        const bl = {
             position: "absolute",
-            left:"0",
-            height:"100%",
-            width:"3px",
-            backgroundColor:"black",
-        }
-        const br={
+            left: "0",
+            height: "100%",
+            width: "3px",
+            backgroundColor: "black",
+        };
+        const br = {
             position: "absolute",
-            right:"0",
-            height:"100%",
-            width:"3px",
-            backgroundColor:"black",
+            right: "0",
+            height: "100%",
+            width: "3px",
+            backgroundColor: "black",
 
-        }
+        };
 
         return (
             <div style={CONTAINER}>
                 <Typography variant="h4" gutterBottom >درباره ی پروژه</Typography>
                 <div style={container}>
                     <div style={imgContainer}>
-                        <img src={img1} style={img}/>
+                        <img src={assets.image1} style={img}/>
                     </div>
                     <div style={info}>
-                        <div style={bl}></div>
-                        <div style={br}></div>
-                        <div style={btl}></div>
-                        <div style={btr}></div>
-                        <div style={bbl}></div>
-                        <div style={bbr}></div>
+                        <div style={bl}/>
+                        <div style={br}/>
+                        <div style={btl}/>
+                        <div style={btr}/>
+                        <div style={bbl}/>
+                        <div style={bbr}/>
                         <div style={explanationBox}>
-                            <p style={{fontSize:"20px",wordBreak:"normal"  }}>
+                            <p style={{fontSize: "20px", wordBreak: "normal"}}>
                                 درباره ی پروژه
                                 <br/>
                                 ....
@@ -183,33 +182,33 @@ class AboutProject extends React.Component {
                 <Typography variant="h4" gutterBottom >اعضای گروه </Typography>
                 <div style={container}>
                     <div style={imgContainer}>
-                        <img src={img1} style={{...img,cursor:"pointer"}}/>
-                        <Typography variant="h6" gutterBottom >محمد حسن ابراهیمی</Typography>
+                        <img src={assets.image1} style={{...img, cursor: "pointer"}}/>
+                        <Typography variant="h6" gutterBottom>محمد حسن ابراهیمی</Typography>
                     </div>
 
                     <div style={imgContainer}>
-                        <img src={img1} style={{...img,cursor:"pointer"}}/>
-                        <Typography variant="h6" gutterBottom >زهرا قائدی</Typography>
+                        <img src={assets.image1} style={{...img, cursor: "pointer"}}/>
+                        <Typography variant="h6" gutterBottom>زهرا قائدی</Typography>
                     </div>
 
                     <div style={imgContainer}>
-                        <img src={img1} style={{...img,cursor:"pointer"}}/>
-                        <Typography variant="h6" gutterBottom >زهرا کمالی</Typography>
+                        <img src={assets.image1} style={{...img, cursor: "pointer"}}/>
+                        <Typography variant="h6" gutterBottom>زهرا کمالی</Typography>
                     </div>
 
                     <div style={imgContainer}>
-                        <img src={img1} style={{...img,cursor:"pointer"}}/>
-                        <Typography variant="h6" gutterBottom >فائزه مرادی</Typography>
+                        <img src={assets.image1} style={{...img, cursor: "pointer"}}/>
+                        <Typography variant="h6" gutterBottom>فائزه مرادی</Typography>
                     </div>
 
                     <div style={imgContainer}>
-                        <img src={img1} style={{...img,cursor:"pointer"}}/>
-                        <Typography variant="h6" gutterBottom >فائزه آقابزرگی</Typography>
+                        <img src={assets.image1} style={{...img, cursor: "pointer"}}/>
+                        <Typography variant="h6" gutterBottom>فائزه آقابزرگی</Typography>
                     </div>
 
                     <div style={imgContainer}>
-                        <img src={img1} style={{...img,cursor:"pointer"}}/>
-                        <Typography variant="h6" gutterBottom >صبا عرب زادگان</Typography>
+                        <img src={assets.image1} style={{...img, cursor: "pointer"}}/>
+                        <Typography variant="h6" gutterBottom>صبا عرب زادگان</Typography>
                     </div>
                 </div>
             </div>
